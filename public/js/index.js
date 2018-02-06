3e91da3780bde1b351b5abb6185fab8c3db5185e
mc4rol9 <mariacarol9@gmail.com>
2018-02-06 21:00:09 -0200

    refactor: Add mustache.js for message templates

--- public/js/index.js
+++ public/js/index.js
@@ -15,29 +15,35 @@ socket.on('disconnect', function() {
 
 // newMessage listener
 socket.on('newMessage', function(message) {
-   var formattedTime = moment(message.createdAt).format('h:mm a');
+    // format time with moment
+    var formattedTime = moment(message.createdAt).format('h:mm a');
+    // render mustache template
+    var template = jQuery('#message-template').html();
+    var html = Mustache.render(template, {
+        text: message.text,
+        from: message.from,
+        createdAt: formattedTime
+    });
 
-    var li = jQuery('<li></li>');
-    li.text(`${message.from} ${formattedTime}: ${message.text}`);
-
-    jQuery('#messages').append(li);
+    jQuery('#messages').append(html);
 });
 
 // newLocationMessage listener
 socket.on('newLocationMessage', function (message) {
+    // format time with moment
     var formattedTime = moment(message.createdAt).format('h:mm a');
+    // render mustache template
+    var template = jQuery('#location-message-template').html();
+    var html = Mustache.render(template, {
+        from: message.from,
+        url: message.url,
+        createdAt: formattedTime
+    });
 
-    var li = jQuery('<li></li>');
-    var a = jQuery('<a target="_blank">My current location</a>');
-
-    li.text(`${message.from} ${formattedTime}: `);
-    a.attr('href', message.url);
-    li.append(a);
-
-    jQuery('#messages').append(li);
+    jQuery('#messages').append(html);
 });
 
-// add submit event listener to form
+// submit event listener to form
 jQuery('#message-form').on('submit', function (e) {
     e.preventDefault();
 
@@ -51,7 +57,7 @@ jQuery('#message-form').on('submit', function (e) {
     });
 });
 
-// add click listener event for geolocation 
+// on click listener event for geolocation 
 var locationButton = jQuery('#send-location');
 locationButton.on('click', function() {
     if (!navigator.geolocation) {
