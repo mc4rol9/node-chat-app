329b3227b8795e42aff5d2313c3131da777a55c9
mc4rol9 <mariacarol9@gmail.com>
2018-02-06 21:42:42 -0200

    feat: Add autoscrolling

--- public/js/index.js
+++ public/js/index.js
@@ -3,6 +3,24 @@
 //
 
 var socket = io();
+
+// autoscrolling calculation
+function scrollToBottom () {
+    // selectors
+    var messages = jQuery('#messages');
+    var newMessage = messages.children('li:last-child');
+    // heights
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+
+    if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+        messages.scrollTop(scrollHeight);
+    }
+};
+
 // connection listener
 socket.on('connect', function() {
     console.log('Connected to server');
@@ -26,6 +44,7 @@ socket.on('newMessage', function(message) {
     });
 
     jQuery('#messages').append(html);
+    scrollToBottom();
 });
 
 // newLocationMessage listener
@@ -41,6 +60,7 @@ socket.on('newLocationMessage', function (message) {
     });
 
     jQuery('#messages').append(html);
+    scrollToBottom();
 });
 
 // submit event listener to form
